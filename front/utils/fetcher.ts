2562b01af658ca2f9d495549af7e5b9f0f77e8ec
정태웅 <jeongtaeung@jeongtaeungs-MacBook-Air.local>
2022-03-17 14:30:25 +0900

    deleted utils

--- front/utils/fetcher.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const fetcher = (url: string) => axios.get(url, {
-    withCredentials: true,
-}). then((response) => {
-    return response.data;
-});
-
-export default fetcher;
